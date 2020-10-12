2fd588fadf54a5867473148b677a1240da75be67
reonaledo <nice2meetyou!>
2020-10-12 20:42:26 +0900

    main.py 돌아가게 만듬

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from src.train import *
 
 
 def main():
-
-    if sys.argv[1] == 'pre':
+    if len(sys.argv) < 2:
+        print('수행할 작업을 선택해주세요. preprocess / train / test')
+    if sys.argv[1] == 'preprocess':
+        print('전처리 시작')
         preprocess()
     if sys.argv[1] == 'train':
+        print('모델 학습 시작')
         train()
     if sys.argv[1] == 'test':
+        print('모델 테스트 시작')
         test()
 
 
